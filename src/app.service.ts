16111bc86c1f2801e12d32c986595a1362ef0079
An Nguyễn <ntanan1996@gmail.com>
2022-06-01 11:03:22 +0700

    update msg

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! This message send from nestjs app';
+    return 'Hello Vue3 + NestJs World!';
   }
 }
