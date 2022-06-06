91064565d44015f30c80deff9221f80e6d16123f
An Nguyễn <ntanan1996@gmail.com>
2022-06-06 10:20:25 +0700

    update

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,9 +1,6 @@
-import * as path from 'path'
-import * as dotenv from 'dotenv'
+// import * as path from 'path'
+require('dotenv/config')
 import type { TypeOrmModuleOptions } from '@nestjs/typeorm'
-
-console.log(path.join(process.cwd(), '.env'))
-dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 const dbConfig: TypeOrmModuleOptions = {
   type: 'postgres',
